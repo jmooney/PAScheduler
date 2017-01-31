a4739873efa745b2738230f2e061924df82baa51
John Mooney <jmooneycs@live.com>
2017-01-31 16:51:03 -0500

    Fix bug when opening in excel on linux; debugging only functionality

--- src/FileManager.py
+++ src/FileManager.py
@@ -3,12 +3,12 @@
 
 	Author:	John Mooney
 	Date:		08/23/2013
-	
+
 	PAScheduler 2 - FileManager
-	
+
 	Description:
 		Manages opening and closing files for the PAScheduler
-		
+
 '''
 
 # Imports
@@ -29,34 +29,34 @@ class FileManager(object):
 		self._schedule 		= schedule
 		self._isAltered 	= True
 		self._workingFile 	= "Untitled"
-		
-		
+
+
 	''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-	
+
 	def newFile(self):
 		if not self._askSaveFile():
 			return
-		
+
 		self._guiMngr.reset()
 		self._schedule.reset()
 		self._workingFile = "Untitled"
-		
-		
+
+
 	def openFile(self):
 		if not self._askSaveFile():
 			return
-			
+
 		filename = filedialog.askopenfilename(defaultextension='.pas', filetypes=[('PA Schedule Project', '.pas'), ('All types', '.*')])
 		if not filename:
 			return
-		
+
 		self._guiMngr.reset()
 		self._schedule.reset()
-			
+
 		self._workingFile = filename
 		with open(filename, 'r') as file:
 			data = [line.rstrip() for line in file.readlines() if line.rstrip()]
-		
+
 		currentPage = None
 		currentPageLines = []
 		for line in data:
@@ -65,47 +65,47 @@ class FileManager(object):
 			else:
 				if currentPage:
 					currentPage.write(currentPageLines, begin=eval(currentPageBegin))
-					
+
 				currentPageName, currentPageBegin = line.lstrip().split('\t')
 				currentPageLines = []
-				
+
 				currentPage = None
 				if currentPageName in self._guiMngr.getPages():
 					currentPage = self._guiMngr.getPage(currentPageName)
 				else:
 					currentPage = self._guiMngr.createPage(currentPageName)
-					
+
 		#	Write trailing Page
 		if currentPage:
 					currentPage.write(currentPageLines, begin=eval(currentPageBegin))
-		
+
 		self._schedule.readSettings()
-				 
+
 
 	def saveFile(self):
 		filename = filedialog.asksaveasfilename(defaultextension='.pas',  filetypes=[('PM Schedule Project', '.pas'), ('All types', '.*')]) if not self._workingFile else self._workingFile
 		if not filename:
 			return False
-			
+
 		with open(filename, 'w') as file:
 			pageBegins = [(1, 0), (1, 1)]
 			for i in range(len(self._guiMngr.getPages())):
 				pageName = self._guiMngr.getPageName(i)
 				pageBegin = pageBegins[i] if i < len(pageBegins) else (0, 0)
 				page = self._guiMngr.getPage(pageName)
-				
+
 				file.write('{}\t{}\n'.format(pageName, pageBegin))
 				self._writePageData(page, file, pageBegin)
-			
+
 		self._workingFile = filename
 		return True
 
-		
+
 	def saveFileAs(self):
 		self._workingFile = ''
 		self.saveFile()
-		
-	
+
+
 	def savePageAs(self, pageId):
 		filename = filedialog.asksaveasfilename(defaultextension='.txt.',  filetypes=[('Text File', '.txt'), ('All types', '.*')])
 		if not filename:
@@ -114,8 +114,8 @@ class FileManager(object):
 		with open(filename, 'w') as file:
 			page = self._guiMngr.getPage(self._guiMngr.getNotebook().tab(pageId, 'text'))
 			self._writePageData(page, file, (0,0), '')
-			
-			
+
+
 	def askQuit(self):
 		if self._isAltered:
 			message = self._askSaveDialog()
@@ -148,21 +148,21 @@ class FileManager(object):
 		# 3. Open the tempfile in Excel
 		from sys import platform as _platform
 		if(_platform == "linux" or _platform == "linux2"):
-			subprocess.Popen(['sublime', tempFilename])
+			subprocess.Popen(['subl', tempFilename])
 			subprocess.Popen(['libreoffice', '--calc', tempFilename])
 		else:
 			subprocess.Popen(["start", "excel.exe", tempFilename], shell=True)
 
 
 	''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-	
+
 	def _askSaveFile(self):
 		if self._isAltered:
 			message = self._askSaveDialog()
 			if (message and not self.saveFile()) or message == None:
 				return False
 		return True
-				
+
 	def _askSaveDialog(self):
 		return messagebox.askyesnocancel(message='Save File ' + basename(self._workingFile) + '?', icon='question', title='Save File?')
 
@@ -173,4 +173,4 @@ class FileManager(object):
 				text+=formatText + col.getRaw() + '\t'
 			if text.lstrip():
 				file.write(text + '\n')
-			
+
